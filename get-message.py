ef978cadaffa56903268ffb292514977daa0e373
Isabel O'Connor <fpt9nh@virginia.edu>
2024-05-02 08:29:33 +0000

    draft

--- get-message.py
+++ get-message.py
@@ -1,12 +1,11 @@
+#!/usr/bin/env python3
 import boto3
 from botocore.exceptions import ClientError
 import requests
 import json
 
-# Set up your SQS queue URL and boto3 client
-url = "https://sqs.us-east-1.amazonaws.com/440848399208/xxxxxxx"
+url = "https://sqs.us-east-1.amazonaws.com/440848399208/fpt9nh"
 sqs = boto3.client('sqs')
-
 def delete_message(handle):
     try:
         # Delete message from SQS queue
@@ -18,40 +17,50 @@ def delete_message(handle):
     except ClientError as e:
         print(e.response['Error']['Message'])
 
+messages_list = [] # create list of messages
+create_list = {} 
+
 def get_message():
-    try:
+    
+    for i in range(10):
+        try:
         # Receive message from SQS queue. Each message has two MessageAttributes: order and word
         # You want to extract these two attributes to reassemble the message
-        response = sqs.receive_message(
-            QueueUrl=url,
-            AttributeNames=[
-                'All'
-            ],
-            MaxNumberOfMessages=1,
-            MessageAttributeNames=[
-                'All'
-            ]
-        )
+            response = sqs.receive_message(
+                QueueUrl=url,
+                AttributeNames=[
+                    'All'
+                ],
+                MaxNumberOfMessages=1,
+                MessageAttributeNames=[
+                    'All'
+                ]
+            )
         # Check if there is a message in the queue or not
-        if "Messages" in response:
+            if "Messages" in response:
             # extract the two message attributes you want to use as variables
             # extract the handle for deletion later
-            order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
-            word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
-            handle = response['Messages'][0]['ReceiptHandle']
+                order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
+                word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
+                handle = response['Messages'][0]['ReceiptHandle']
+
+                create_list.append(handle)
+                messages_list[order] = word
 
             # Print the message attributes - this is what you want to work with to reassemble the message
-            print(f"Order: {order}")
-            print(f"Word: {word}")
+                print(f"Order: {order}")
+                print(f"Word: {word}")
 
         # If there is no message in the queue, print a message and exit    
-        else:
-            print("No message in the queue")
-            exit(1)
+            else:
+                print("No message in the queue")
+                exit(1)
             
     # Handle any errors that may occur connecting to SQS
-    except ClientError as e:
-        print(e.response['Error']['Message'])
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+
+    print(messages_list)
 
 # Trigger the function
 if __name__ == "__main__":
